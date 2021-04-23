0c283bef20599753018d9e7521399f4ea0454171
Alexey Sokolovskiy <alexej.sokolovskiy@gmail.com>
2021-04-23 15:56:00 +0300

    Added infix notation and file support to CLI, changed to jcommander

--- src/main/java/CLI.java
+++ src/main/java/CLI.java
@@ -1,31 +1,56 @@
-import org.apache.commons.cli.*;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.JCommander;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CLI {
+    @Parameter(names = {"-e", "--expression"}, description = "expression to evaluate")
+    private List<String> expressions = new ArrayList<>();
+
+    @Parameter(names = {"-f", "--file"}, description = "name of file with expressions")
+    private List<String> filenames = new ArrayList<>();
+
+    @Parameter(names = {"--help", "-h"}, help = true)
+    private boolean help = false;
+
     public static void main(String[] args) {
-        Options options = new Options();
-        Option expression_arg = new Option("e", "evaluate", true, "Expression to evaluate");
-        // now this is only possible scenario
-        expression_arg.setRequired(true);
-        options.addOption(expression_arg);
-
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = null;
-
+        CLI main = new CLI();
+        JCommander jc = JCommander.newBuilder()
+                .addObject(main)
+                .build();
         try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("utility-name", options);
-
-            System.exit(1);
+            jc.parse(args);
         } catch (Exception e) {
-            System.out.println("Unexpected exception: " + e.getMessage());
+            jc.usage();
             System.exit(1);
         }
+        if (main.help) {
+            jc.usage();
+            return;
+        }
+        main.run();
+    }
 
-        String expression = cmd.getOptionValue(expression_arg.getOpt());
-        Calculator calc = new Calculator(Notation.Postfix);
-        System.out.println(calc.Evaluate(expression));
+    void run() {
+        boolean success = true;
+        Calculator calc = new Calculator(Notation.Infix);
+        for (String expression : expressions) {
+            try {
+                System.out.println(calc.Evaluate(expression));
+            } catch (Exception e) {
+                success = false;
+                System.out.println();
+                System.err.printf("Exception %s occurred, message: %s %n", e.getClass().getName(), e.getMessage());
+            }
+        }
+        StreamEvaluator evaluator = new StreamEvaluator(calc);
+        for (String filename : filenames) {
+            success &= evaluator.EvaluateFile(filename);
+        }
+        if (!success) {
+            System.exit(1);
+        }
     }
 }
